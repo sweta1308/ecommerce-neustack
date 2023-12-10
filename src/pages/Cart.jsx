61fba55fa1212474b503be805a0f540d2808fa8f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-10 17:27:38 +0530

    feat: add cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,37 @@
+import { ClipLoader } from "react-spinners";
+import { useCart } from "../context/CartContext";
+import { EmptyCart } from "../components/EmptyCart";
+import { CartCard } from "../components/CartCard";
+import { PriceCard } from "../components/PriceCard";
+
+export const Cart = () => {
+  const { cartState } = useCart();
+  return (
+    <div className="px-[40px] py-[20px] bg-gray-50 mx-auto lg:px-[20px]">
+      <h1 className="text-center text-[28px] font-semibold">Cart</h1>
+      {cartState?.isLoading ? (
+        <div className="flex justify-center mt-[20px]">
+          <ClipLoader color="#e80071" size={70} />
+        </div>
+      ) : cartState?.cart.length === 0 ? (
+        <EmptyCart />
+      ) : (
+        <div className="flex justify-around mt-[20px] px-[70px] lg:px-[20px] md:flex-col md:items-center">
+          <div>
+            {cartState?.cart.map(({ _id, item, price, quantity }) => (
+              <CartCard
+                id={_id}
+                item={item}
+                price={price}
+                quantity={quantity}
+              />
+            ))}
+          </div>
+          <div>
+            <PriceCard />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
