9a644e8945074ff74cbf829448909f8858e748fe
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-10 17:26:05 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import { Products } from "./pages/Products";
+import { Navbar } from "./components/Navbar";
+import { Cart } from "./pages/Cart";
+import { Checkout } from "./pages/Checkout";
+import { OrderPlaced } from "./pages/OrderPlaced";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: "1rem",
+          right: "1rem",
+          fontSize: "0.9rem",
+        }}
+      />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-placed" element={<OrderPlaced />} />
+      </Routes>
     </div>
   );
 }
