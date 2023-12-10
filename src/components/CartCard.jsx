8b0555e017f12f1ea822335333894d0ba05ed3fa
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-10 17:26:14 +0530

    feat: add cart card

---
+++ src/components/CartCard.jsx
@@ -0,0 +1,42 @@
+import { Add, Delete, Remove } from "@mui/icons-material";
+import { useCart } from "../context/CartContext";
+
+export const CartCard = ({ id, item, price, quantity }) => {
+  const { updateQuantity, deleteItem } = useCart();
+  return (
+    <div
+      key={id}
+      className="w-[400px] m-[20px] flex justify-evenly items-center gap-10 p-[30px] shadow-md bg-white rounded-md xs:flex-col xs:gap-3 xs:w-[300px]"
+    >
+      <div>
+        <h4 className="font-bold xs:text-center">{item}</h4>
+        <p className="xs:text-center">Price: ₹{price}</p>
+      </div>
+      <div className="flex items-center text-[14px] gap-5">
+        <div className="flex justify-evenly items-center gap-2">
+          <button
+            disabled={quantity === 1}
+            onClick={() => updateQuantity(id, "decrement")}
+            className="border border-gray-300 px-[5px] py-[2px] cursor-pointer rounded-full hover:bg-gray-200"
+          >
+            <Remove sx={{ fontSize: "16px" }} />
+          </button>
+          <p>{quantity}</p>
+          <button
+            onClick={() => updateQuantity(id, "increment")}
+            className="border border-gray-300 px-[5px] py-[2px] cursor-pointer rounded-full hover:bg-gray-200"
+          >
+            <Add sx={{ fontSize: "16px" }} />
+          </button>
+        </div>
+
+        <div
+          onClick={() => deleteItem(id)}
+          className="text-red-500 cursor-pointer"
+        >
+          <Delete />
+        </div>
+      </div>
+    </div>
+  );
+};
